5d6a79a84bcbb15642a70808992f0193f137aa86
thomashirsch <thomashirsch@ad1ff917-242d-0410-93f6-b1940bde3b1a>
2007-05-11 09:09:15 +0000

    javadoc: DataSource

--- trunk/net/relet/freimap/DataSource.java
+++ trunk/net/relet/freimap/DataSource.java
@@ -24,16 +24,63 @@ package net.relet.freimap;
 
 import java.util.*;
 
+/** The DataSource interface specifies a single source of node, link and related 
+    data. It will be called by the Freimap core classes, i.e. {@link VisorFrame} and 
+    {@link Visor} in the following general order:
+    <ol>
+      <li>Usually, a {@link DataSourceListener} is registered first.</li>
+      <li>Then, by calling getFirst- and getLastUpdateTime, the initial time interval 
+          to be displayed is determined. </li>
+      <li>A call to getNodeList should return an initial Vector of {@link Freinode}s.</li>
+      <li>Optionally, getNodeAvailability may return an availability value for each node. 
+          That's a bit of a hack, and more general node properties will be implemented soon.</li>
+    </ol>
+    The initiation phase is thus concluded. In the following, the core will repeatedly 
+    request getClosestUpdateTime for unix timestamps which it seeks to display. Ususally, 
+    this time value will then be used to retrieve the actual link data from getLinks. 
+    The core may also request to retrieve a LinkInfo profile (for FreiLinks) or a NodeProfile
+    (for FreiNodes) using the methods getLinkProfile and getLinkCountProfile (to be renamed). 
+    These methods may directly alter the LinkInfo and NodeInfo objects and set the appropriate 
+    status flags.
+    At any time, the methods in {@link DataSourceListener} may be called by the DataSource to 
+    extend the available time range or add new nodes. 
+*/
+
 public interface DataSource {
+
+/** @return	A Vector of FreiNodes to be displayed. */
   public Vector<FreiNode> getNodeList();
+/** <b>To be deprecated soon.</b>
+    @param	time	ignored in current implementations.
+    @return	A Hashtable mapping Node IDs to a float value between 0 and 1.
+*/
   public Hashtable<String, Float> getNodeAvailability(long time);
-  public long getLastUpdateTime();
+/** @return	The first unix timestamp which can be displayed. */
   public long getFirstUpdateTime();
+/** @return	The last unix timestamp which can be displayed. */
+  public long getLastUpdateTime();
+/** This method approximates a requested time with the closest timestamp which can actually be 
+    displayed.
+    @param	time	A timestamp to be approximated 
+    @return	the next best timestamp.
+*/
   public long getClosestUpdateTime(long time);
+/** Returns link data for a given timestamp. May return null if the timestamp has not been 
+    provided by getClosestUpdateTime.
+    @param	time	A timestamp
+    @return	All links which should be displayed at this given time.
+*/
   public Vector<FreiLink> getLinks(long time);
-  //threaded information fetching
+/** @param 	dsl	A DataSourceListener listening on events from this DataSource */
   public void addDataSourceListener(DataSourceListener dsl);
-  //some optional methods
+/** Extendend link information is requested using this method. 
+    @param	link	the link on which information is sought
+    @param	info	A LinkInfo structure to be filled with this information. 
+*/
   public void getLinkProfile(FreiLink link, LinkInfo info); 
+/** Extendend node information is requested using this method. 
+    @param	node	the node on which information is sought
+    @param	info	A NodeInfo structure to be filled with this information. 
+*/
   public void getLinkCountProfile(FreiNode node, NodeInfo info); 
 }
